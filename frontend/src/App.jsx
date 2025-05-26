38ebc4617c0353a7455252fe1b550c4976afb8d4
fFukurou <supevi1@gmail.com>
2025-05-25 22:52:39 -0300

    changed background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     // we are not using the 'forest' theme explicitly here, but it is declared in tailwind.config.js
     // https://bg.ibelick.com/
     <div className="relative h-full w-full">
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#1d9d31] bg-[radial-gradient(#ffffff33_1px,#000000_1px)] bg-[size:20px_20px]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
